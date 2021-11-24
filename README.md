b0b3a45d512fd695c69f62bda6454e59b5f0572f
evan <evandibona@gmx.com>
2021-11-24 14:09:54 -0500

    added assets, factored js

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## To Do
 
-1) Condense the interface and calls. Token could be generated with no clicks. 
+1) Move js into separate file with high order functions. Factor out ajax calls. 
---
+++ js/api.js
@@ -0,0 +1,55 @@
+var Api = {}
+Api.rootUrl = ""
+Api.domain  = ""
+Api.header  = {}
+
+
+Api.VerifyToken = function(fx){
+  $.ajax({
+    url: Api.domain + "/api/v1/accounts/verify_credentials",
+    type: "GET", 
+    headers: Api.header,  
+    success: fx,
+  })
+}
+
+Api.AppInit = function(fx){
+  $.post(Api.domain + '/api/v1/apps', 
+  {
+    client_name: "AccountPortal", 
+    redirect_uris: "URN:Ietf:wg:oauth:2.0:oob"
+  }, 
+  fx 
+  ); 
+}
+
+Api.LoginURL = function(id){
+  return Api.domain + '/oauth/authorize' +
+    '?client_id=' + id +
+    '&scope=read+write' +
+    '&redirect_uri=' + Api.rootUrl +
+    '&response_type=code'
+}
+
+Api.GetToken = function(id, st, code, fx){
+  $.post(Api.domain + "/oauth/token", 
+  {
+    client_id: id,
+    client_secret: st,
+    redirect_uri: Api.rootUrl,
+    grant_type: 'authorization_code', 
+    code: code, 
+    scope: 'read write'
+  },
+  fx )
+}
+
+Api.PasswordChange = function(oldpass, newpass, fx){
+  $.ajax({
+    url: Api.domain + "/api/v1/user/password_change",
+    type: "POST", 
+    headers: Api.header, 
+    data: { old_password: oldpass, new_password: newpass },
+    success: fx
+  }); 
+}
